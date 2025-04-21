46e6c9361024a320c8f43bfd8fec4411a288ad42
GuillemotClement <guillemot.clement@protonmail.com>
2025-04-21 14:08:10 +0200

    FEAT: REPL

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
-)
-
-func cleanInput(text string) []string {
-	cleanInput := strings.Trim(text, " ")
-	lowerInput := strings.ToLower(cleanInput)
-	stringText := strings.Split(lowerInput, " ")
-	return stringText
-}
-
 func main() {
-	// recupere la saisis clavier d'un user
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for true {
-		fmt.Print("Pokedex >")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())
-		inputClean := input[0]
-
-		fmt.Printf("Your command was: %s\n", inputClean)
-	}
+	startRepl()
 }
--- repl_test.go
+++ repl_test.go
@@ -1,37 +1,44 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestCleanInput(t *testing.T) {
-	// creation d'un slice de test
 	cases := []struct {
 		input    string
 		expected []string
 	}{
 		{
-			// input de la fonction
-			input: " Hello World ",
-			// resultat en sortie
+			input:    "  ",
+			expected: []string{},
+		},
+		{
+			input:    "  hello  ",
+			expected: []string{"hello"},
+		},
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HellO  World  ",
 			expected: []string{"hello", "world"},
 		},
 	}
-	// ajout d'autre cas a tester
 
-	// iteration sur le contenu
 	for _, c := range cases {
-		// on passe sur les case definis, et on les passe dans la fonction
 		actual := cleanInput(c.input)
 		if len(actual) != len(c.expected) {
-			t.Errorf("Length mismatch for input %v: got %v, want %v", c.input, len(actual), len(c.expected))
+			t.Errorf("lengths don't match: '%v' vs '%v'", actual, c.expected)
 			continue
 		}
 		for i := range actual {
 			word := actual[i]
 			expectedWord := c.expected[i]
 			if word != expectedWord {
-				t.Errorf("Word mismatch at position %v for input %v: got %v, want %v", i, c.input, word, expectedWord)
+				t.Errorf("cleanInput(%v) == %v, expected %v", c.input, actual, c.expected)
 			}
 		}
 	}
-
 }
