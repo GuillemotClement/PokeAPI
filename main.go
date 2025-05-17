131ad73b5e16b245f66fd26e6011319e69942fac
GuillemotClement <guillemot.clement@protonmail.com>
2025-05-17 14:24:43 +0200

    correction

--- main.go
+++ main.go
@@ -1,4 +1,6 @@
 package main
 
 func main() {
+	// appel de la fonction pour lancer le repl
+	StartRepl()
 }
--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -11,4 +14,32 @@ func CleanInput(text string) []string {
 	words := strings.Fields(output)
 	// on retourne le slice
 	return words
+}
+
+func StartRepl() {
+	// creation du reader
+	reader := bufio.NewScanner(os.Stdin)
+	// creation d'une boucle pour recup les input
+	for {
+		// print pour inviter de commande
+		fmt.Print("Pokedex > ")
+		// on viens scanner l'input
+		reader.Scan()
+
+		// clean input recuperer
+		// reader.Text() => recupere l'input scanner
+		words := CleanInput(reader.Text())
+		// gestion erreur
+		if len(words) == 0 {
+			// si rien on redemande un nouvel input
+			continue
+		}
+
+		// on viens recuperer le premier mot de l'input
+		commandName := words[0]
+
+		// retour pour user
+		fmt.Printf("Your commande was: %s\n", commandName)
+	}
+
 }
